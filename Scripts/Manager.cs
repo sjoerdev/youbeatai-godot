2c0291ad20767d9826a80aa766a8675a376a987d
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-10-24 15:08:16 +0200

    make circle smaller

--- Scripts/Manager.cs
+++ Scripts/Manager.cs
@@ -155,7 +155,7 @@ public partial class Manager : Node
     {
         var sprite = (Sprite2D)spritePrefab.Instantiate();
         float angle = Mathf.Pi * 2 * beat / beatsAmount - Mathf.Pi / 2;
-        float distance = (4 - ring) * 34 + 136;
+        float distance = (4 - ring) * 30 + 110;
         sprite.Position = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * distance;
         sprite.Texture = texture;
 
