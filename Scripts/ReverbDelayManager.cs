a2bd3fff98a907d63a25fa76891a3e12b5160c61
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-10-24 12:15:17 +0200

    reverb and delay

---
+++ Scripts/ReverbDelayManager.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class ReverbDelayManager : Node
+{
+	[Export] Button reverbButton;
+	[Export] Button delayButton;
+
+	[Export] Sprite2D reverbSprite;
+	[Export] Sprite2D delaySprite;
+
+	AudioEffectReverb reverbEffect;
+    AudioEffectDelay delayEffect;
+
+    private int currentReverbLevel = 0;
+    private int currentDelayLevel = 0;
+    private float[] levels = { 0.0f, 0.5f, 1.0f };
+
+    public override void _Ready()
+    {
+        reverbButton.Pressed += OnReverbButtonPressed;
+        delayButton.Pressed += OnDelayButtonPressed;
+        reverbEffect = new AudioEffectReverb();
+        delayEffect = new AudioEffectDelay();
+        AudioServer.AddBusEffect(AudioServer.GetBusIndex("Master"), reverbEffect);
+        AudioServer.AddBusEffect(AudioServer.GetBusIndex("Master"), delayEffect);
+        SetReverbLevel(levels[currentReverbLevel]);
+        SetDelayLevel(levels[currentDelayLevel]);
+    }
+
+    private void OnReverbButtonPressed()
+    {
+        currentReverbLevel = (currentReverbLevel + 1) % levels.Length;
+        SetReverbLevel(levels[currentReverbLevel]);
+    }
+
+	private void OnDelayButtonPressed()
+    {
+        currentDelayLevel = (currentDelayLevel + 1) % levels.Length;
+        SetDelayLevel(levels[currentDelayLevel]);
+    }
+
+    private void SetReverbLevel(float level)
+    {
+        reverbEffect.RoomSize = level;
+		reverbSprite.Scale = Vector2.One * level;
+    }
+
+    private void SetDelayLevel(float level)
+    {
+        delayEffect.Dry = 1 - level;
+		delaySprite.Scale = Vector2.One * level;
+    }
+}
